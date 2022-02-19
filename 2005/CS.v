a55fc286c5cc7b7adabf89250be302623d7cfd97
GaryChen886 <76957777+GaryChen886@users.noreply.github.com>
2022-02-20 01:00:20 +0800

    Update CS.v

--- 2005/CS.v
+++ 2005/CS.v
@@ -4,35 +4,40 @@ module CS(Y, X, reset, clk);
 	input clk, reset;
 	input 	[7:0] X;
 	output reg [9:0] Y;
-
-	reg [7:0] XS [8:0];
-	reg [13:0] sum;
-	reg [7:0] avg; 
-	reg [7:0] appr; 
-	integer i,j;
 //--------------------------------------
 //  \^o^/   Write your code here~  \^o^/
 //--------------------------------------
+    reg [7:0] XS [8:0]; //XSay of 8-bit number with length 9
+	reg [13:0] sum; //sum of XSay
+	reg [7:0] avg; //average of sum
+	reg [7:0] appr; //approximate average
+	integer i,j,k;
 	always @(posedge clk or posedge reset) begin
 		if(reset)begin
-			for(i=0;i<9;i=i+1)
+			for(i=0;i<9;i=i+1)begin
 				XS[i] <= 0;
+			end
 			sum <= 0;
 			i <= 0;
 		end
 		else begin
 			sum <= sum - XS[0] + X;
-			for(i=0;i<8;i=i+1)
-				XS[i] <= XS[i+1];
+
+			for(j=0;j<8;j=j+1)begin
+				XS[j] <= XS[j+1];
+			end
 			XS[8] = X;
 		end
 	end
 	always @(*) begin
 		appr = 0;
 		avg = sum/9;
-		for(j=0;j<9;j=j+1)
-			if(`abs(XS[j],avg) < `abs(appr,avg))
-				appr = XS[j];
-		Y = (sum + {appr,3'b0})>>3;
+		for(k=0;k<9;k=k+1)begin
+			if((avg >= XS[k]) && (`abs(avg,XS[k]) < `abs(appr,avg)))begin
+				appr = XS[k];
+			end
+		end
+		Y = (sum + (appr*9))/8;
 	end
+
 endmodule
