3321bd29bd664462585e7e36a65bbaca53be068c
GaryChen886 <76957777+GaryChen886@users.noreply.github.com>
2022-02-19 23:07:55 +0800

    Update CS.v

--- 2005/CS.v
+++ 2005/CS.v
@@ -1,44 +1,38 @@
 `timescale 1ns/10ps
 `define abs(a,b) ((a>b)? a-b : b-a )
 module CS(Y, X, reset, clk);
+	input clk, reset;
+	input 	[7:0] X;
+	output reg [9:0] Y;
 
-    input clk, reset; 
-    input [7:0]X;
-    output reg [9:0] Y;
-  //--------------------------------------
-  //  \^o^/   Write your code here~  \^o^/
-  //--------------------------------------
-    integer i, j;
-    reg [7:0] XS [9:1] ;   //8bits * 9
-    reg [12:0] sum;
-    reg [7:0] avg;
-    reg [7:0] appr;
-    reg [3:0] cnt;
-    
-    always @(posedge clk or posedge reset)begin
-        if(reset)begin
-            for(i = 1;i <= 9;i = i+1)
-                XS[i] <= 0;
-            sum <= 0;
-            avg <= 0;
-            appr <= 0;
-            i <= 0;
-            cnt <= 0;
-        end
-        else begin
-            for(i = 9;i > 2;i = i - 1)
-                XS[i] <= XS[i-1];
-            XS[1] <= X;
-            sum <= sum - XS[9] + X;
-        end
-    end
-    always @(*)begin
-        avg = sum / 9;
-        appr = XS[1];
-        for(j = 2; j <= 9;j = j + 1)begin
-            if( `abs(avg,XS[j])<`abs(avg,appr) )
-                appr = XS[j];
-        end
-        Y = (sum + appr<<3 + appr)/8;
-    end
+	reg [7:0] XS [8:0];
+	reg [13:0] sum;
+	reg [7:0] avg; 
+	reg [7:0] appr; 
+	integer i,j;
+//--------------------------------------
+//  \^o^/   Write your code here~  \^o^/
+//--------------------------------------
+	always @(posedge clk or posedge reset) begin
+		if(reset)begin
+			for(i=0;i<9;i=i+1)
+				XS[i] <= 0;
+			sum <= 0;
+			i <= 0;
+		end
+		else begin
+			sum <= sum - XS[0] + X;
+			for(i=0;i<8;i=i+1)
+				XS[i] <= XS[i+1];
+			XS[8] = X;
+		end
+	end
+	always @(*) begin
+		appr = 0;
+		avg = sum/9;
+		for(j=0;j<9;j=j+1)
+			if(`abs(XS[j],avg) < `abs(appr,avg))
+				appr = XS[j];
+		Y = (sum + {appr,3'b0})>>3;
+	end
 endmodule
