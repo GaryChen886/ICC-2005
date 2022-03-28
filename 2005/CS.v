b235806a41445613dba8665c8fea7d78dabbad16
GaryChen886 <76957777+GaryChen886@users.noreply.github.com>
2022-03-28 18:49:08 +0800

    Update CS.v

--- 2005/CS.v
+++ 2005/CS.v
@@ -1,41 +1,43 @@
-`timescale 1ns/10ps
-`define abs(a,b) ((a>b)? a-b : b-a )
 module CS(Y, X, reset, clk);
-	input clk, reset;
-	input 	[7:0] X;
-	output reg [9:0] Y;
-//--------------------------------------
-//  \^o^/   Write your code here~  \^o^/
-//--------------------------------------
-    reg [7:0] XS [8:0]; //XSay of 8-bit number with length 9
-	reg [13:0] sum; //sum of XSay
-	reg [7:0] avg; //average of sum
-	reg [7:0] appr; //approximate average
-	integer i,j,k;
-	always @(posedge clk or posedge reset) begin
-		if(reset)begin
-			for(i=0;i<9;i=i+1)begin
-				XS[i] <= 0;
-			end
-			sum <= 0;
-		end
-		else begin
-			sum <= sum - XS[0] + X;
+    input clk, reset; 
+    input 	[7:0] X;
+    output reg [9:0] Y;
+    //--------------------------------------
+    //  \^o^/   Write your code here~  \^o^/
+    //--------------------------------------
 
-			for(j=0;j<8;j=j+1)begin
-				XS[j] <= XS[j+1];
-			end
-			XS[8] = X;
-		end
-	end
-	always @(*) begin
-		appr = 0;
-		avg = sum/9;
-		for(k=0;k<9;k=k+1)begin
-			if((avg >= XS[k]) && (`abs(avg,XS[k]) < `abs(appr,avg)))begin
-				appr = XS[k];
-			end
-		end
-		Y = (sum + (appr>>9 + appr))>>3;
-	end
+    reg [7:0] X_Series[8:0];   
+    reg [10:0] sum;
+
+    generate
+        genvar i;
+        always @(posedge clk or posedge reset)begin
+            if(reset)begin
+                for(i=0;i<9;i=i+1)begin
+                    X_Series[i]<=0;
+                end
+                sum<=0;
+            end
+            else begin
+                for(i=8;i>0;i=i-1)
+                    X_Series[i]<=X_Series[i-1];
+                X_Series[0]<=X;
+                sum<=sum-X_Series[8]+X;
+            end
+            
+        end
+    endgenerate
+
+    genvar j;
+    reg [8:0] Xappr;
+    generate
+        always @(*)begin
+            Xappr=0;
+            for(j=0;j<9;j=j+1)begin
+                if((X_Series[j]<=sum/9)&&(X_Series[j]>Xappr))  
+                    Xappr=X_Series[j];
+            end
+            Y=(sum+({Xappr,3'b0}+Xappr))>>3;
+        end
+    endgenerate
 endmodule
